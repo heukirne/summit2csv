7f574c86dfc67ca2c9453d18fa0d900b62230c84
bolipereira <bolivar042@gmail.com>
2017-05-12 18:35:30 -0300

    12/05/17(2)

--- programa.py
+++ programa.py
@@ -1,44 +1,44 @@
-lista = {}	
+lista = {}
 x = 1
 contSent = 1
 contTok = 1
-contText = 1
-strLine = ""	
+contText = ""
+strLine = ""
 with open("Summ-it++v2.txt", "r", encoding="iso-8859-1") as file:
-	for line in file:
-	    if line == "#end document\n":
-	        contSent = 1
-	        contText = contText + 1
-	    if line == "\n":
-	        if strLine.find("class='org'") >= 0:
-	            if strLine.find("class='org'", strLine.find("class='org") + 1) >= 0:
-	                lista[x] = strLine
-	                x = x + 1
-	            elif strLine.find("class='per'") >= 0:
-	                lista[x] = strLine
-	                x = x + 1
-	            elif strLine.find("class='per'") >= 0:
-	                lista[x] = strLine
-	                x = x + 1
-	        contTok = 1
-	        contSent = contSent + 1
-	        strLine = ""
-	    columns = line.split('\t')
-	    if len(columns) == 9: 
-	        if columns[6] == "(PLC" or columns[6] == "(PLC)" or columns[6] == "PLC)" or columns[6] == "PLC":
-	            strLine += "<span class='entity' class='plc' style='color:red' id='tok"+str(contTok)+"sent"+str(contSent)+"text"+str(contText)+"'>"+columns[1]+"</span> "
-	            contTok = contTok + 1
-	        elif columns[6] == "(ORG" or columns[6] == "(ORG)" or columns[6] == "ORG)" or columns[6] == "ORG":
-	            strLine += "<span class='entity' class='org' style='color:blue' id='tok"+str(contTok)+"sent"+str(contSent)+"text"+str(contText)+"'>"+columns[1]+"</span> "
-	            contTok = contTok + 1
-	        elif columns[6] == "(PER" or columns[6] == "(PER)" or columns[6] == "PER)" or columns[6] == "PER":
-	            strLine += "<span class='entity' class='per' style='color:green' id='tok"+str(contTok)+"sent"+str(contSent)+"text"+str(contText)+"'>"+columns[1]+"</span> "
-	            contTok = contTok + 1
-	        elif len(columns[6]) > 1:
-	            strLine += "<span class='entity' id='tok"+str(contTok)+"sent"+str(contSent)+"text"+str(contText)+"'>"+columns[1]+"</span> "
-	        else:
-	            strLine += "<span class='word' id='tok"+str(contTok)+"sent"+str(contSent)+"text"+str(contText)+"'>"+columns[1]+"</span> "
-	            contTok = contTok + 1
+    for line in file:
+        columns = line.split('\t')
+        if line.find("#begin") >= 0:
+            columns = line.split('/')
+            contText = columns[7]
+        if line == "\n":
+            if strLine.find("class='org'") >= 0:
+                if strLine.find("class='org'", strLine.find("class='org") + 1) >= 0:
+                    lista[x] = strLine
+                    x = x + 1
+                elif strLine.find("class='per'") >= 0:
+                    lista[x] = strLine
+                    x = x + 1
+                elif strLine.find("class='per'") >= 0:
+                    lista[x] = strLine
+                    x = x + 1
+            contTok = 1
+            contSent = contSent + 1
+            strLine = ""
+        if len(columns) == 9: 
+            if columns[6] == "(PLC" or columns[6] == "(PLC)" or columns[6] == "PLC)" or columns[6] == "PLC":
+                strLine += "<span class='entity' class='plc' style='color:red' id='tok"+str(contTok)+"sent"+str(contSent)+"text"+contText+"'>"+columns[1]+"</span> "
+                contTok = contTok + 1
+            elif columns[6] == "(ORG" or columns[6] == "(ORG)" or columns[6] == "ORG)" or columns[6] == "ORG":
+                strLine += "<span class='entity' class='org' style='color:blue' id='tok"+str(contTok)+"sent"+str(contSent)+"text"+contText+"'>"+columns[1]+"</span> "
+                contTok = contTok + 1
+            elif columns[6] == "(PER" or columns[6] == "(PER)" or columns[6] == "PER)" or columns[6] == "PER":
+                strLine += "<span class='entity' class='per' style='color:green' id='tok"+str(contTok)+"sent"+str(contSent)+"text"+contText+"'>"+columns[1]+"</span> "
+                contTok = contTok + 1
+            elif len(columns[6]) > 1:
+                strLine += "<span class='entity' id='tok"+str(contTok)+"sent"+str(contSent)+"text"+contText+"'>"+columns[1]+"</span> "
+            else:
+                strLine += "<span class='word' id='tok"+str(contTok)+"sent"+str(contSent)+"text"+contText+"'>"+columns[1]+"</span> "
+                contTok = contTok + 1
 	
 with open("saida.txt", "a") as myfile:
 	y = 1
